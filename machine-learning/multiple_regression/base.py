913371dba23ae48a65d0a00b5c793e57791c599e
Samir Madhavan <samir.madhavan@gmail.com>
2019-07-10 12:54:27 +0530

    Add multiple linear regression

--- machine-learning/multiple_regression/base.py
+++ machine-learning/multiple_regression/base.py
@@ -1,9 +1,9 @@
 import csv
+import numpy as np
 
-
-class SimpleLinearRegression:
+class MultipleLinearRegression:
     def __init__(self, epoch=3, batch_size=15, lr=0.001):
-        self.weight = 0
+        self.weight = []
         self.bias = 0
         self.epoch = epoch
         self.batch_size = batch_size
@@ -11,6 +11,7 @@ class SimpleLinearRegression:
 
     def fit(self, X, Y):
         num_batch = len(X)//self.batch_size + 1
+        self.weight = np.zeros((len(X[0])))
 
         for i in range(self.epoch):
             for j in range(num_batch):
@@ -18,22 +19,24 @@ class SimpleLinearRegression:
                                     ,Y[j*self.batch_size: (j+1)*self.batch_size])
 
     def sgd(self, x_batch, y_batch):
-        dCw = 0.0
-        dCb = 0.0
+        dCw = np.zeros((self.weight.shape[0]))
+        dCb = 0
         bs = len(x_batch)
 
         for i in range(bs):
-            dCw +=  (self.weight*x_batch[i] + self.bias - y_batch[i])*x_batch[i]
-            dCb +=  (self.weight*x_batch[i] + self.bias - y_batch[i])
+            y_pred = self.predict_single(x_batch[i])
+            y_actual = y_batch[i]
+            diff = y_pred - y_actual
+
+            for j in range(self.weight.shape[0]):
+                dCw[j] += diff*x_batch[i][j]
+            dCb +=  diff
 
         dCw = (2*dCw)/bs
         dCb = (2*dCb)/bs
 
         self.weight -=  self.lr * dCw
         self.bias -=  self.lr * dCb
-
-        self.weight = clamp(self.weight, -500,500)
-        self.bias = clamp(self.bias, -500,500)
 
     def cost(self, X, Y):
         y_pred = self.predict(X)
@@ -49,23 +52,13 @@ class SimpleLinearRegression:
         self.sgd(x_batch, y_batch)
 
     def predict(self, x):
-        return [ self.predict_single(e) for e in x]
+        return [ self.predict_single(row) for row in x]
 
     def predict_single(self, x_single):
-        return self.weight*x_single + self.bias
+        return sum(self.weight*x_single) + self.bias
 
-def clamp(n, minn, maxn):
-    return max(min(maxn, n), minn)
-
-def read_data(filename):
-    result = []
-
-    with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for i, row in enumerate(csv_reader):
-            if i == 0:
-                continue
-            result.append(row)
-
-    return result
+def feature_scale(X, x_min, x_max):
+    nom = (X-X.min(axis=0))*(x_max-x_min)
+    denom = X.max(axis=0) - X.min(axis=0)
+    denom[denom==0] = 1
+    return x_min + nom/denom
---
+++ machine-learning/multiple_regression/main.py
@@ -0,0 +1,21 @@
+from sklearn.linear_model import LinearRegression
+from base import MultipleLinearRegression, feature_scale
+from sklearn.datasets import load_boston
+import numpy as np
+
+data = load_boston()
+Y = data['target']
+X = data['data']
+X = feature_scale(X, -1, 1)
+
+model = MultipleLinearRegression(epoch=10000, lr=0.001, batch_size=20)
+model.fit(X, Y)
+
+print('----MultipleLinearRegression----')
+print(f'Weight: {model.weight} Bias: {model.bias}')
+print('\n')
+
+sklearn_model = LinearRegression()
+sklearn_model.fit( X,Y)
+print('----Sklearn LinearRegression----')
+print(f'Weight: {sklearn_model.coef_} Bias: {sklearn_model.intercept_}')
